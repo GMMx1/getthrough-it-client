16d54935031e146e9f89bedeb33b8f89d332d275
Minh Nguyen <kuroemon95@gmail.com>
2017-07-24 17:42:40 -0500

    start to refactor ChallengeInfo

--- src/components/ChallengeInfo.js
+++ src/components/ChallengeInfo.js
@@ -1,2 +1,25 @@
 import React, {Component} from 'react'
-import Tabs from 'react-tabs-component'
+import TestTable from '../components/TestTable'
+import Tabs from 'react-simpletabs'
+class ChallengeInfo extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-bold centered">{this.props.currentChallenge.name}</div>
+        <div>{this.props.currentChallenge.question}</div>
+        {!!this.props.currentChallenge &&
+          <div className="test-suite">
+            <TestTable
+              tests={this.props.currentChallenge.input_output || []}
+              sandboxResult={this.props.sandboxResult || []} />
+            </div> }
+      </div>
+    )
+  }
+}
+
+export default ChallengeInfo
